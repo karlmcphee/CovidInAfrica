6ab55ae9759e125bb78af269c8a845105ba55c19
karlmcphee <32080714+karlmcphee@users.noreply.github.com>
2023-05-21 08:10:58 -0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.bfb07cf75bab8fa1e32b24b491c31a03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc8b9a721427f160c6fc",
+    "url": "/react-gh-pages/static/css/main.3ab6eae9.chunk.css"
+  },
+  {
+    "revision": "cc8b9a721427f160c6fc",
+    "url": "/react-gh-pages/static/js/main.cc8b9a72.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/react-gh-pages/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/react-gh-pages/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "a6558f0c85422d431291",
+    "url": "/react-gh-pages/static/js/runtime~main.a6558f0c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-gh-pages/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "fca740d9818e29742a241351ce83d391",
+    "url": "/react-gh-pages/index.html"
+  }
+];
